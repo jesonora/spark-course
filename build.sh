bc7499e0bd6b3dce05367323e4608e59d011f724
Jesus Sono Razuri <jesusmanuel.sono@allianz.es>
2021-11-02 18:48:34 +0100

    Adding new files

---
+++ build.sh
@@ -0,0 +1,29 @@
+# -- Software Stack Version
+
+SPARK_VERSION="3.1.2"
+HADOOP_VERSION="2.7"
+JUPYTER_VERSION="1.0.0"
+
+echo ${SPARK_VERSION}
+echo ${HADOOP_VERSION}
+echo ${JUPYTER_VERSION}
+
+# -- Building the Images
+
+docker build \
+  -f conf/cluster-base.Dockerfile \
+  -t cluster-base .
+
+docker build \
+  --build-arg spark_version="${SPARK_VERSION}" \
+  --build-arg hadoop_version="${HADOOP_VERSION}" \
+  -f conf/spark-base.Dockerfile \
+  -t spark-base .
+
+docker build \
+  -f conf/spark-master.Dockerfile \
+  -t spark-master:1.0.0 .
+
+docker build \
+  -f conf/spark-worker.Dockerfile \
+  -t spark-worker:1.0.0 .
